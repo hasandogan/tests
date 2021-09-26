f0844d8aa6abf14e81637b99057f55a756e31746
hasan <hasandoganx@gmail.com>
2021-09-26 03:39:17 +0300

    new project

---
+++ src/Controller/sayacController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+class sayacController extends AbstractController
+{
+    /**
+     * @Route("/",name="index")
+     */
+    public function index(){
+        return $this->render('sayac.html.twig');
+    }
+
+}
