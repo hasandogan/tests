5e3a79aee18ee2000497e103999acea94be09452
hasan <hasandoganx@gmail.com>
2021-09-29 15:24:32 +0300

    dolar kuru kaldirildi

--- src/Controller/sayacController.php
+++ src/Controller/sayacController.php
@@ -11,33 +11,10 @@ class sayacController extends AbstractController
      * @Route("/",name="secim-sayaci")
      */
     public function secimSayaci(){
-        $url = "https://www.dolarbey.com";
-        $linkData = file_get_contents($url,false, stream_context_create(['https' => ['ignore_errors' => true]]));
+        return $this->render('sayac.html.twig');
 
-      $dolar = $this->getDolar($linkData);
-      $euro = $this->getEuro($linkData);
-      $pound = $this->getPound($linkData);
-        return $this->render('sayac.html.twig', [
-            'dolar' => $dolar,
-            'euro' => $euro,
-            'pound' => $pound
-        ]);
     }
-    public function getDolar($linkData){
-        $data = preg_match_all('@<div data-socket-key="USD" data-socket-type="C" data-socket-attr="s" class="text-xl font-semibold text-white">(.*?)</div>@si',$linkData,$response);
-        $dolar = $response[1][0];
-            return $dolar;
-    }
-    public function getEuro($linkData){
-        $data = preg_match_all('@<div data-socket-key="EUR" data-socket-type="C" data-socket-attr="s" class="text-xl font-semibold text-white">(.*?)</div>@si',$linkData,$response);
-        $euro = $response[1][0];
-        return $euro;
-    }
-    public function getPound($linkData){
-        $data = preg_match_all('@<div data-socket-key="GBP" data-socket-type="C" data-socket-attr="s" class="text-xl font-semibold text-white">(.*?)</div>@si',$linkData,$response);
-        $euro = $response[1][0];
-        return $euro;
-    }
+
 
     /**
      * @Route ("/new-year","new-year")
