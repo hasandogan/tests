da98087c66654decb1d4bef0094d46fb02021eb3
YasinAkdeniz <yasnakdeniz@gmail.com>
2021-10-03 22:39:41 +0300

    Register sayfası yapıldı

--- src/Controller/LoginAuthController.php
+++ src/Controller/LoginAuthController.php
@@ -7,28 +7,50 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
+
 class LoginAuthController extends AbstractController
 {
     /**
      * @Route ("/loginAuth")
      **/
-    public function loginAuthCeheck(Request $request){
+    public function loginAuthCeheck(Request $request)
+    {
 
-         $email =  $request->get('email');
-        $password =$request->get('password');
+        $passwords = sha1($request->request->get('password'));
+        $email = $request->get('email');
         $entityManager = $this->getDoctrine()->getManager();
         $userRepo = $entityManager->getRepository(User::class)->findOneBy([
-            'email' =>$email,
-            'password' => $password
+            'email' => $email,
+            'password' => $passwords
         ]);
-    if ($userRepo){
-        $session = new Session();
-        $session->set('email', $email);
-        $session->set('id', $userRepo->getId());
-     return $this->redirect("/profile");
-    }else{
-    return  $this->redirect("/");
+        if ($userRepo) {
+            $session = new Session();
+            $session->set('email', $email);
+            $session->set('id', $userRepo->getId());
+            return $this->redirect("/profile");
+        } else {
+            return $this->redirect("/");
+        }
     }
+
+    /**
+     * @Route ("/registerAuth")
+     **/
+    public function registerAuth(Request $request)
+    {
+        $password1 = sha1($request->request->get('password1'));
+        $password2 = sha1($request->request->get('password2'));
+        if ($password1 === $password2) {
+            $user = new User ();
+            $user->setEmail($request->request->get('email'));
+            $user->setPassword($password1);
+            $this->getDoctrine()->getManager()->persist($user);
+            $this->getDoctrine()->getManager()->flush();
+            return $this->redirect("/login");
+
+        } else {
+            return $this->redirect("/register");
+        }
     }
 
 }
