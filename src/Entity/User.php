7a2ca002d6a3b80b9811d270b92aeb27513063ed
Hasan Doğan <69388854+hasandogan@users.noreply.github.com>
2021-10-06 11:51:40 +0300

    Update User.php

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,6 +5,9 @@ namespace App\Entity;
 use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Symfony\Component\Validator\Constraints as Assert;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
