01196af3d78bae4ab82739871b0b2b4ef712616b
hasan <hasandoganx@gmail.com>
2021-11-08 19:23:59 +0300

    düzenleme

--- public/ganl.js
+++
@@ -1,9 +0,0 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-BVCGJW43RG"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-BVCGJW43RG');
-</script>
