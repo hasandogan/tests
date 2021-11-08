96d21df36ad86adf50b5047ad661477aa41b6540
Hasan Doğan <69388854+hasandogan@users.noreply.github.com>
2021-11-08 10:10:12 +0300

    Update ApiController.php

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -113,8 +113,11 @@ class ApiController extends AbstractController
     public function countDownById(Request $request){
         $parameter = json_decode($request->getContent(),true);
         $id = $parameter['id'];
+                        var_dump($id);
         $entityManager = $this->getDoctrine()->getManager();
           $counter = $entityManager->getRepository(CustomeCounter::class)->findOneBy(['id' => $id]);
+                var_dump($counter);
+
           $counterArray = [];
             foreach ($counter as $count ){
                 $counterArray[] = ['id' => $count->getId(),'userId' => $count->getUser()->getId(),'name' => $count->getName(),'firstText' => $count->getTextFirst(),'lastText' => $count -> getTextLast(),'dateTime' => $count ->getDateTime()];
