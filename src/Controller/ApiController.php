b9d1924cbc37efef1129f7d4ef2ccfc090983a9e
Hasan Doğan <69388854+hasandogan@users.noreply.github.com>
2021-11-08 09:18:11 +0300

    Update ApiController.php

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -103,8 +103,7 @@ class ApiController extends AbstractController
          foreach ($customCheck as $count ){
             $counterArray[] = ['id' => $count->getId(),'userId' => $count->getUser()->getId(),'name' => $count->getName(),'firstText' => $count->getTextFirst(),'lastText' => $count -> getTextLast(),'dateTime' => $count ->getDateTime()];
         }
-        dd($counterArray);exit;
-        return new Response(json_encode($customCheck));
+        return new Response(json_encode($counterArray));
     }
 
 }
