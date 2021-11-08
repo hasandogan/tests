fd47976859a4ec7a5ff6a11975b85998822c7679
Hasan Doğan <69388854+hasandogan@users.noreply.github.com>
2021-11-08 14:32:20 +0300

    Create ganl.js

---
+++ public/ganl.js
@@ -0,0 +1,9 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-BVCGJW43RG"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-BVCGJW43RG');
+</script>
